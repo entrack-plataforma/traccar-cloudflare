a01e3e73eacc83477e5f4301b0b3176a5251573f
jcardus <joaquim.cardeira@gmail.com>
2024-08-06 11:23:15 +0100

    @capacitor/android

--- build-ios.sh
+++ build-ios.sh
@@ -3,5 +3,5 @@ npx cap add ios
 mkdir assets
 cp -v ../assets/* assets
 npx capacitor-assets generate
-mkdir ios/App/fastlane ios/App/assets
+mkdir ios/App/fastlane
 cp -v ../fastlane/* ios/App/fastlane
